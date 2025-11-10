16106f4c69f87daaa3c2d40bd75cd37d65d1a92d
tsmorz <tonysmoragiewicz@gmail.com>
2025-11-10 15:36:50 +0100

    fix type hint

--- src/py_imu/quaternion.py
+++ src/py_imu/quaternion.py
@@ -9,238 +9,6 @@ from numpy.typing import NDArray
 from py_imu.definitions import EPSILON
 
 
-class Quaternion:
-    """Quaternion Class.
-
-    q1 = Quaternion(1., 2., 3., 4.)
-    q2 = Quaternion(w=5., x=6., y=7., z=8.)
-    q3 = Quaternion(np.array([9,10,11,12]))
-
-    q5 = q1 + q2
-    q6 = q1 * q2
-    q7 = 2 * q1
-
-    q1.conjugate
-    q1.inverse
-    q1.normalize()
-    q1.r33 (cosine matrix)
-    q1.norm: length of quaternion
-    q1.v: vector part of quaternion as Vector3D
-    q1.q: quaternion as np.array
-    """
-
-    def __init__(self, w=0.0, x=0.0, y=0.0, z=0.0, v=None):
-        # allows any possible combination to be passed in
-        if v is None:
-            if isinstance(w, numbers.Number):
-                self.w = w
-                self.x = x
-                self.y = y
-                self.z = z
-
-    def __str__(self):
-        return f"Quaternion({self.w}, {self.x}, {self.y}, {self.z})"
-
-    def __repr__(self):
-        return str(self)
-
-    def __add__(self, other):
-        """Add two quaternions or quaternion and scalar."""
-        if isinstance(other, Quaternion):
-            return Quaternion(
-                self.w + other.w, self.x + other.x, self.y + other.y, self.z + other.z
-            )
-        elif isinstance(other, np.ndarray):
-            return Quaternion(
-                self.w + other[0],
-                self.x + other[1],
-                self.y + other[2],
-                self.z + other[3],
-            )
-        elif isinstance(other, numbers.Number):
-            return Quaternion(
-                self.w + other, self.x + other, self.y + other, self.z + other
-            )
-        else:
-            raise TypeError(
-                f"Unsupported operand type for +: Quaternion and {type(other)}"
-            )
-
-    def __sub__(self, other):
-        """Subtract two quaternions or quaternion and scalar."""
-        if isinstance(other, Quaternion):
-            return Quaternion(
-                self.w - other.w, self.x - other.x, self.y - other.y, self.z - other.z
-            )
-        elif isinstance(other, np.ndarray):
-            return Quaternion(
-                self.w - other[0],
-                self.x - other[1],
-                self.y - other[2],
-                self.z - other[3],
-            )
-        elif isinstance(other, numbers.Number):
-            return Quaternion(
-                self.w - other, self.x - other, self.y - other, self.z - other
-            )
-        else:
-            raise TypeError(
-                f"Unsupported operand type for -: Quaternion and {type(other)}"
-            )
-
-    def __mul__(self, other):
-        """Multiply two quaternions or quaternion and vector."""
-        if isinstance(other, Quaternion):
-            w = (
-                (self.w * other.w)
-                - (self.x * other.x)
-                - (self.y * other.y)
-                - (self.z * other.z)
-            )
-            x = (
-                (self.w * other.x)
-                + (self.x * other.w)
-                + (self.y * other.z)
-                - (self.z * other.y)
-            )
-            y = (
-                (self.w * other.y)
-                - (self.x * other.z)
-                + (self.y * other.w)
-                + (self.z * other.x)
-            )
-            z = (
-                (self.w * other.z)
-                + (self.x * other.y)
-                - (self.y * other.x)
-                + (self.z * other.w)
-            )
-            return Quaternion(w, x, y, z)
-        elif isinstance(other, Vector3D):
-            """
-            multiply quaternion with vector
-            vector is converted to quaternion with [0,vector]
-            then computed the same as above with other.w=0
-            """
-            w = -(self.x * other.x) - (self.y * other.y) - (self.z * other.z)
-            x = self.w * other.x + self.y * other.z - self.z * other.y
-            y = self.w * other.y - self.x * other.z + self.z * other.x
-            z = self.w * other.z + self.x * other.y - self.y * other.x
-            return Quaternion(w, x, y, z)
-        elif isinstance(other, numbers.Number):
-            """multiply with scalar"""
-            return Quaternion(
-                self.w * other, self.x * other, self.y * other, self.z * other
-            )
-        else:
-            raise TypeError("Unsupported operand type")
-
-    def __rmul__(self, other):
-        """Right multiplication."""
-        return self.__mul__(other)
-
-    def __truediv__(self, other):
-        """Division."""
-        if isinstance(other, numbers.Number):
-            return Quaternion(
-                self.w / other, self.x / other, self.y / other, self.z / other
-            )
-        else:
-            raise TypeError("Unsupported operand type")
-
-    def __floordiv__(self, other):
-        """Floor division."""
-        if isinstance(other, numbers.Number):
-            return Quaternion(
-                self.w // other, self.x // other, self.y // other, self.z // other
-            )
-        else:
-            raise TypeError("Unsupported operand type")
-
-    def __eq__(self, other):
-        """Check that the two quaternions are equal."""
-        return (
-            self.w == other.w
-            and self.x == other.x
-            and self.y == other.y
-            and self.z == other.z
-        )
-
-    def normalize(self):
-        """Normalize the quaternion."""
-        mag = self.norm
-        if mag != 0:
-            self.w = self.w / mag
-            self.x = self.x / mag
-            self.y = self.y / mag
-            self.z = self.z / mag
-
-    @property
-    def v(self) -> NDArray:
-        """Extract the vector component of the quaternion."""
-        # return np.array([self.x,self.y,self.z])
-        return Vector3D(self.x, self.y, self.z)
-
-    @property
-    def q(self) -> NDArray:
-        """Convert the quaternion to np.array."""
-        # return np.array([self.x,self.y,self.z])
-        return np.array([self.w, self.x, self.y, self.z])
-
-    @property
-    def conjugate(self):
-        """Conjugate of quaternion."""
-        return Quaternion(self.w, -self.x, -self.y, -self.z)
-
-    @property
-    def norm(self) -> float:
-        """Length of quaternion."""
-        return math.sqrt(
-            self.w * self.w + self.x * self.x + self.y * self.y + self.z * self.z
-        )
-
-    @property
-    def inverse(self):
-        """Inverse of quaternion."""
-        return self.conjugate / self.norm
-
-    @property
-    def r33(self) -> NDArray:
-        """Quaternion to 3x3 rotation matrix."""
-        # Normalize quaternion
-        # self.normalize()
-
-        # Compute rotation matrix elements
-        xx = self.x * self.x
-        xy = self.x * self.y
-        xz = self.x * self.z
-        xw = self.x * self.w
-        yy = self.y * self.y
-        yz = self.y * self.z
-        yw = self.y * self.w
-        zz = self.z * self.z
-        zw = self.z * self.w
-
-        # Construct the rotation matrix
-        return np.array(
-            [
-                [1.0 - 2.0 * (yy + zz), 2.0 * (xy - zw), 2.0 * (xz + yw)],
-                [2.0 * (xy + zw), 1.0 - 2.0 * (xx + zz), 2.0 * (yz - xw)],
-                [2.0 * (xz - yw), 2.0 * (yz + xw), 1.0 - 2.0 * (xx + yy)],
-            ]
-        )
-
-    @property
-    def isZero(self) -> bool:
-        """Check whether the quaternion is zero."""
-        return (
-            abs(self.w) <= EPSILON
-            and abs(self.x) <= EPSILON
-            and abs(self.y) <= EPSILON
-            and abs(self.z) <= EPSILON
-        )
-
-
 class Vector3D:
     """3D Vector Class.
 
@@ -465,4 +233,236 @@ class Vector3D:
         """Check if vector is zero."""
         return (
             abs(self.x) <= EPSILON and abs(self.y) <= EPSILON and abs(self.z) <= EPSILON
+        )
+
+
+class Quaternion:
+    """Quaternion Class.
+
+    q1 = Quaternion(1., 2., 3., 4.)
+    q2 = Quaternion(w=5., x=6., y=7., z=8.)
+    q3 = Quaternion(np.array([9,10,11,12]))
+
+    q5 = q1 + q2
+    q6 = q1 * q2
+    q7 = 2 * q1
+
+    q1.conjugate
+    q1.inverse
+    q1.normalize()
+    q1.r33 (cosine matrix)
+    q1.norm: length of quaternion
+    q1.v: vector part of quaternion as Vector3D
+    q1.q: quaternion as np.array
+    """
+
+    def __init__(self, w=0.0, x=0.0, y=0.0, z=0.0, v=None):
+        # allows any possible combination to be passed in
+        if v is None:
+            if isinstance(w, numbers.Number):
+                self.w = w
+                self.x = x
+                self.y = y
+                self.z = z
+
+    def __str__(self):
+        return f"Quaternion({self.w}, {self.x}, {self.y}, {self.z})"
+
+    def __repr__(self):
+        return str(self)
+
+    def __add__(self, other):
+        """Add two quaternions or quaternion and scalar."""
+        if isinstance(other, Quaternion):
+            return Quaternion(
+                self.w + other.w, self.x + other.x, self.y + other.y, self.z + other.z
+            )
+        elif isinstance(other, np.ndarray):
+            return Quaternion(
+                self.w + other[0],
+                self.x + other[1],
+                self.y + other[2],
+                self.z + other[3],
+            )
+        elif isinstance(other, numbers.Number):
+            return Quaternion(
+                self.w + other, self.x + other, self.y + other, self.z + other
+            )
+        else:
+            raise TypeError(
+                f"Unsupported operand type for +: Quaternion and {type(other)}"
+            )
+
+    def __sub__(self, other):
+        """Subtract two quaternions or quaternion and scalar."""
+        if isinstance(other, Quaternion):
+            return Quaternion(
+                self.w - other.w, self.x - other.x, self.y - other.y, self.z - other.z
+            )
+        elif isinstance(other, np.ndarray):
+            return Quaternion(
+                self.w - other[0],
+                self.x - other[1],
+                self.y - other[2],
+                self.z - other[3],
+            )
+        elif isinstance(other, numbers.Number):
+            return Quaternion(
+                self.w - other, self.x - other, self.y - other, self.z - other
+            )
+        else:
+            raise TypeError(
+                f"Unsupported operand type for -: Quaternion and {type(other)}"
+            )
+
+    def __mul__(self, other):
+        """Multiply two quaternions or quaternion and vector."""
+        if isinstance(other, Quaternion):
+            w = (
+                (self.w * other.w)
+                - (self.x * other.x)
+                - (self.y * other.y)
+                - (self.z * other.z)
+            )
+            x = (
+                (self.w * other.x)
+                + (self.x * other.w)
+                + (self.y * other.z)
+                - (self.z * other.y)
+            )
+            y = (
+                (self.w * other.y)
+                - (self.x * other.z)
+                + (self.y * other.w)
+                + (self.z * other.x)
+            )
+            z = (
+                (self.w * other.z)
+                + (self.x * other.y)
+                - (self.y * other.x)
+                + (self.z * other.w)
+            )
+            return Quaternion(w, x, y, z)
+        elif isinstance(other, Vector3D):
+            """
+            multiply quaternion with vector
+            vector is converted to quaternion with [0,vector]
+            then computed the same as above with other.w=0
+            """
+            w = -(self.x * other.x) - (self.y * other.y) - (self.z * other.z)
+            x = self.w * other.x + self.y * other.z - self.z * other.y
+            y = self.w * other.y - self.x * other.z + self.z * other.x
+            z = self.w * other.z + self.x * other.y - self.y * other.x
+            return Quaternion(w, x, y, z)
+        elif isinstance(other, numbers.Number):
+            """multiply with scalar"""
+            return Quaternion(
+                self.w * other, self.x * other, self.y * other, self.z * other
+            )
+        else:
+            raise TypeError("Unsupported operand type")
+
+    def __rmul__(self, other):
+        """Right multiplication."""
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        """Division."""
+        if isinstance(other, numbers.Number):
+            return Quaternion(
+                self.w / other, self.x / other, self.y / other, self.z / other
+            )
+        else:
+            raise TypeError("Unsupported operand type")
+
+    def __floordiv__(self, other):
+        """Floor division."""
+        if isinstance(other, numbers.Number):
+            return Quaternion(
+                self.w // other, self.x // other, self.y // other, self.z // other
+            )
+        else:
+            raise TypeError("Unsupported operand type")
+
+    def __eq__(self, other):
+        """Check that the two quaternions are equal."""
+        return (
+            self.w == other.w
+            and self.x == other.x
+            and self.y == other.y
+            and self.z == other.z
+        )
+
+    def normalize(self):
+        """Normalize the quaternion."""
+        mag = self.norm
+        if mag != 0:
+            self.w = self.w / mag
+            self.x = self.x / mag
+            self.y = self.y / mag
+            self.z = self.z / mag
+
+    @property
+    def v(self) -> Vector3D:
+        """Extract the vector component of the quaternion."""
+        # return np.array([self.x,self.y,self.z])
+        return Vector3D(self.x, self.y, self.z)
+
+    @property
+    def q(self) -> NDArray:
+        """Convert the quaternion to np.array."""
+        # return np.array([self.x,self.y,self.z])
+        return np.array([self.w, self.x, self.y, self.z])
+
+    @property
+    def conjugate(self):
+        """Conjugate of quaternion."""
+        return Quaternion(self.w, -self.x, -self.y, -self.z)
+
+    @property
+    def norm(self) -> float:
+        """Length of quaternion."""
+        return math.sqrt(
+            self.w * self.w + self.x * self.x + self.y * self.y + self.z * self.z
+        )
+
+    @property
+    def inverse(self):
+        """Inverse of quaternion."""
+        return self.conjugate / self.norm
+
+    @property
+    def r33(self) -> NDArray:
+        """Quaternion to 3x3 rotation matrix."""
+        # Normalize quaternion
+        # self.normalize()
+
+        # Compute rotation matrix elements
+        xx = self.x * self.x
+        xy = self.x * self.y
+        xz = self.x * self.z
+        xw = self.x * self.w
+        yy = self.y * self.y
+        yz = self.y * self.z
+        yw = self.y * self.w
+        zz = self.z * self.z
+        zw = self.z * self.w
+
+        # Construct the rotation matrix
+        return np.array(
+            [
+                [1.0 - 2.0 * (yy + zz), 2.0 * (xy - zw), 2.0 * (xz + yw)],
+                [2.0 * (xy + zw), 1.0 - 2.0 * (xx + zz), 2.0 * (yz - xw)],
+                [2.0 * (xz - yw), 2.0 * (yz + xw), 1.0 - 2.0 * (xx + yy)],
+            ]
+        )
+
+    @property
+    def isZero(self) -> bool:
+        """Check whether the quaternion is zero."""
+        return (
+            abs(self.w) <= EPSILON
+            and abs(self.x) <= EPSILON
+            and abs(self.y) <= EPSILON
+            and abs(self.z) <= EPSILON
         )
